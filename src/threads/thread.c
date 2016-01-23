c30b7f7ae3b8f37cc7ef34e1986fffdd32f08c09
rluk001 <rluk001@ucr.edu>
2016-01-22 17:52:58 -0800

    l

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -513,6 +513,8 @@ init_thread (struct thread *t, const char *name, int priority)
   t->basePriority = priority;
   t->magic = THREAD_MAGIC;
   list_push_back (&all_list, &t->allelem);
+
+  list_init(&t->lockList);
 }
 
 /* Allocates a SIZE-byte frame at the top of thread T's stack and
