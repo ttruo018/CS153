0486304c46f442c415edb56b3fb5b98c6391e2ce
rluk001 <rluk001@ucr.edu>
2016-01-25 16:22:21 -0800

    fixed

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -363,10 +363,11 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
   ASSERT (lock_held_by_current_thread (lock));
 
   if (!list_empty (&cond->waiters))
-{ 
+  { 
     list_sort(&cond->waiters, &semPriority, NULL); // sort to get highest priority
     sema_up (&list_entry (list_pop_front (&cond->waiters),
                           struct semaphore_elem, elem)->semaphore);
+  }
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
