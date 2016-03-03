aec5ab0bf5e50db023b3d76fbe2d0e2758a948c2
rluk001 <rluk001@ucr.edu>
2016-03-02 22:40:41 -0800

    sys_exit for read

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -344,7 +344,7 @@ int fd_open(const char * file)
 	struct file * fileOpen = filesys_open(file);
 	if(fileOpen == NULL)
 	{
-		return -1;
+		sys_exit(-1);
 	}
 	
 	struct fd_elem * hash = malloc(sizeof(struct fd_elem));
