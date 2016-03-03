a947cd68263ec0dbe04fd2c9e6c0580623ae0771
rluk001 <rluk001@ucr.edu>
2016-03-03 01:46:42 -0800

    all of open works now

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -390,7 +390,7 @@ int fd_open(const char * file)
 
 static int sys_open(const char *file)
 {
-	if(file == NULL) // || !verify_user(file))
+	if(file == NULL || !verify_user(file))
 	{
 		sys_exit(-1);
 	}
