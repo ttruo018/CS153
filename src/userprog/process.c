4869fe735bddade7f47662f69d97abd95ff245df
rluk001 <rluk001@ucr.edu>
2016-03-03 17:51:53 -0800

    correct stuff

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -263,6 +263,7 @@ process_exit (void)
     struct list_elem *e;
     struct list_elem *f;
     struct child_process *curProcess;
+    file_close(cur->execFile);
 
     if(cur->wait != NULL)
     {
@@ -278,7 +279,6 @@ process_exit (void)
     }
 
     free_open_files(thread_current());
-    file_close(cur->execFile);
 
     pd = cur->pagedir;
     if (pd != NULL) 
